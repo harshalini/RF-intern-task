e4a395bf0da2df926d9913ed7bd46f1ca2ff75a9
Harshalini <harshalinipandhare29@gmail.com>
2022-07-25 15:14:21 +0530

    setup: router

---
+++ src/router/router.js
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom"
+import { Home } from "../pages"
+export const AppRouter = () => {
+    return (
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+        </Routes>
+    )
+}
